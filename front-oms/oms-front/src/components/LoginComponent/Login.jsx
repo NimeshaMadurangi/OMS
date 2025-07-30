e76718103ee0ce0111edee95073b1e300c60a9b5
nimesha8 <maduranginimesha8@gmail.com>
2025-07-30 12:32:58 +0530

    design login page

---
+++ front-oms/oms-front/src/components/LoginComponent/Login.jsx
@@ -0,0 +1,28 @@
+import "./Login.css"
+
+function Login() {
+  return (
+    <div>
+      <form class="form">
+        <p class="form-title">Sign in to your account</p>
+        <div class="input-container">
+          <input type="email" placeholder="Enter email" />
+          <span></span>
+        </div>
+        <div class="input-container">
+          <input type="password" placeholder="Enter password" />
+        </div>
+        <button type="submit" class="submit">
+          Sign in
+        </button>
+
+        <p class="signup-link">
+          No account?
+          <a href="">Sign up</a>
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default Login
